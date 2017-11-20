f51539ded8c7950a8dd78bb936fcd2bb976e8bed
Farida Eid <farida.eid@aucgypt.edu>
2017-11-20 18:29:24 +0200

    Testing UDPsocket in main2

--- main2.cpp
+++ main2.cpp
@@ -29,13 +29,33 @@ int main() {
     
     MarshalledMessage marshalledMessage = recievedMessage;
     
-    std::vector<CustomObject* > returnValues = {new CustomString()};
+    std::vector<CustomObject* > returnValues = {new CustomString(), new CustomInt(), new CustomVector(), new CustomBool()};
     
     unmarshal(marshalledMessage, returnValues);
     
     CustomString * returnValueString = dynamic_cast<CustomString *>(returnValues[0]);
+    
+    CustomInt * returnValueInt = dynamic_cast<CustomInt *>(returnValues[1]);
+    
+    CustomVector * returnValueVector = dynamic_cast<CustomVector *>(returnValues[2]);
+    
+    CustomBool * returnValueBool = dynamic_cast<CustomBool *>(returnValues[3]);
 
     std::cout<<returnValueString->getValue()<<std::endl;
+    
+    std::cout<<returnValueInt->getValue()<<std::endl;
+    
+    for(int i = 0; i < returnValueVector->getValue().size(); i++) {
+        
+        std::cout<<returnValueVector->getValue()[i]<<std::endl;
+        
+    }
+    
+    if (returnValueBool->getValue()) {
+        std::cout<<"Value is true"<<std::endl;
+    } else {
+        std::cout<<"Value is false"<<std::endl;
+    }
     
     
     
