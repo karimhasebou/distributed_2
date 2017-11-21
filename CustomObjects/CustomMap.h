2a4cef182ce0950a43eff558c8ae36d059c906c6
kareefardi <kareemfarid@aucegypt.edu>
2017-11-21 14:38:26 +0200

    added custom map

---
+++ CustomObjects/CustomMap.h
@@ -0,0 +1,24 @@
+#ifndef CustomMap_h
+#define CustomMap_h
+
+#include <map>
+#include <string>
+#include "CustomObject.h"
+class CustomMap : public CustomObject {
+private:
+	using Table = std::map<std::string, int>;
+	//std::map<std::string, int> table;
+	int getValueAt(int);
+	std::string getKeyAt(int);
+	Table table;
+public:
+	CustomMap(){};
+	CustomMap(Table);
+	//CustomMap(std::map<std::string, int>);
+	~CustomMap();
+	virtual std::string marshal();
+	virtual int unmarshal(char*, const int&);
+	Table getValue();
+};
+
+#endif
