bbc16df6656da7fbe3baf0216328c7b062ccdd62
karimhasebou <karimhasebou@aucegypt.edu>
2017-11-25 19:15:45 +0200

    added stubs & calls for server

--- RPC/RpcCalls.h
+++ RPC/RpcCalls.h
@@ -9,8 +9,13 @@
 #ifndef RpcCalls_h
 #define RpcCalls_h
 
+#include <stdlib.h>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <cstdio>
+#include <set>
+#include <map>
 #include "../CustomObjects/CustomObject.h"
 #include "../CustomObjects/CustomInt.h"
 #include "../CustomObjects/CustomString.h"
@@ -20,25 +25,60 @@
 #include "../Marshalling/MarshallingManager.h"
 #include "../UDPLayer/Message.h"
 
-enum LoginStatus {
+#define IMG_DIR ""
+#define IMG_LIST_FILE ""
+#define AUTHORIZED_USERS ""
 
+enum LoginStatus {
     loginSucess = 1,
     wrongPassword = 2,
     wrongUsername = 3
 };
 
+namespace server{
+    typedef struct{
+        char *content;
+        size_t length;
+    } Image;
+
+    typedef struct{
+        std::string username;
+        int viewCount;
+    } DBEntry;
+
+    //utilities
+    std::map<std::string, int> readAuthorizedUsersDB();
+    void writeDB(std::map<std::string, int> list);
+
+    std::vector<std::string> listImages();
+    std::set<std::string> getAuthorizedUsers(std::string filePath);
+    
+    void server::stegUserList(std::string imagename);
+    void unstegUserList(std::string filePath);
+    std::string getPath(std::string imgName);
+
+    Image getImage(std::string imgName);
+    std::vector<std::string> getAccessibleImages(std::string username);
+
+    bool canUpdateCount(std::string imgName, 
+        std::string username);
+    bool updateCount(std::string imgName, 
+        std::string username, int count);
+
+    // returns empty string if 
+    // no user is currently logged in
+    std::string getLoggedUsername();
+}
+
 namespace client {
     
     LoginStatus login(std::string, std::string);
     std::vector<std::string> getIPAddress();
-    std::string getUsername(std::string);
-    
 }
 
 namespace authserver {
     LoginStatus login(std::string, std::string);
     std::vector<std::string> getIPAddress();
-    std::string getUsername(std::string);
 }
 
 
--- RPC/RpcStubServer.cpp
+++ RPC/RpcStubServer.cpp
@@ -187,33 +187,4 @@ Message getAccessibleImages(Message msg)
     replyMessage.setRpcOperation(msg.getRpcOperation());
     
     return replyMessage;
-    
-    // string username;
-    // CustomString* mName;
-    // MarshalledMessage mmsg;
-    // vector <CustomObject*> args = {new CustomString()};
-    // mmsg = (MarshalledMessage)packet.getPacketMessage();
-    // unmarshal(mmsg, args);
-    // mName = dynamic_cast<CustomString*>(args[0]);
-    // username = mName->getValue();
-    
-    // vector<string> imageList;
-    // imageList = getAccessibleImages(username);
-    
-    // CustomVector* returnVal = new CustomVector();
-    // for (string& img : imageList) {
-    // 	returnVal->push_back(img);
-    // }
-    // vector <CustomObject*> reply = {returnVal}; // to be checked
-    // MarshalledMessage rmmsg;
-    // marshal(rmmsg, reply);
-    // Message rmsg(rmmsg);
-    // rmsg.setRpcOperation(0);
-    // rmsg.setRpcRequestID(0);
-    
-    // MySocket socket;
-    // int port = 40000;
-    // socket.bind(port);
-    // packet.setPacketMessage(rmsg);
-    // socket.reply(packet);
 }
