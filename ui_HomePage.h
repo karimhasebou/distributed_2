7d0a744b8fe52986b27b8fce8a5e40a753db2ff7
faridaeid <farida.eid@aucegypt.edu>
2017-11-28 15:26:23 +0200

    Changes in UI

--- ui_HomePage.h
+++ ui_HomePage.h
@@ -14,42 +14,40 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
 #include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QLineEdit>
-#include <QtWidgets/QListView>
+#include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
 
-class Ui_HomePage
+class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QWidget *gridLayoutWidget_2;
-    QGridLayout *gridLayout_2;
-    QLabel *label_3;
-    QLineEdit *usernameToUpdateEdit;
-    QLineEdit *viewsToUpdateEdit;
-    QPushButton *update_views;
-    QLabel *label_2;
-    QWidget *gridLayoutWidget_3;
-    QGridLayout *gridLayout_3;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QHBoxLayout *horizontalLayout;
+    QPushButton *allImagesButton;
+    QPushButton *myImagesButton;
+    QPushButton *ediImageButton;
     QPushButton *requestImageButton;
-    QPushButton *viewImageButton;
-    QLabel *imageStatusLabel;
-    QLabel *viewsNumLabel;
-    QListView *imagesList;
-    QPushButton *getImagesButton;
-    QWidget *gridLayoutWidget;
-    QGridLayout *gridLayout;
-    QListView *my_images;
-    QLabel *label;
-    QLabel *Image_holder;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *imageView;
+    QListWidget *imagesList;
+    QGridLayout *editImageEntries;
+    QLineEdit *usernameLabel;
+    QLabel *label_3;
+    QLabel *label_2;
+    QLineEdit *viewCountLabel;
+    QPushButton *updateEditsButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -60,103 +58,105 @@ public:
         HomePage->resize(800, 600);
         centralwidget = new QWidget(HomePage);
         centralwidget->setObjectName(QStringLiteral("centralwidget"));
-        gridLayoutWidget_2 = new QWidget(centralwidget);
-        gridLayoutWidget_2->setObjectName(QStringLiteral("gridLayoutWidget_2"));
-        gridLayoutWidget_2->setGeometry(QRect(590, 10, 192, 218));
-        gridLayout_2 = new QGridLayout(gridLayoutWidget_2);
-        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
-        gridLayout_2->setContentsMargins(0, 0, 0, 0);
-        label_3 = new QLabel(gridLayoutWidget_2);
-        label_3->setObjectName(QStringLiteral("label_3"));
-
-        gridLayout_2->addWidget(label_3, 1, 0, 1, 1);
-
-        usernameToUpdateEdit = new QLineEdit(gridLayoutWidget_2);
-        usernameToUpdateEdit->setObjectName(QStringLiteral("usernameToUpdateEdit"));
-
-        gridLayout_2->addWidget(usernameToUpdateEdit, 0, 1, 1, 1);
-
-        viewsToUpdateEdit = new QLineEdit(gridLayoutWidget_2);
-        viewsToUpdateEdit->setObjectName(QStringLiteral("viewsToUpdateEdit"));
-
-        gridLayout_2->addWidget(viewsToUpdateEdit, 1, 1, 1, 1);
-
-        update_views = new QPushButton(gridLayoutWidget_2);
-        update_views->setObjectName(QStringLiteral("update_views"));
-
-        gridLayout_2->addWidget(update_views, 2, 0, 1, 2);
-
-        label_2 = new QLabel(gridLayoutWidget_2);
-        label_2->setObjectName(QStringLiteral("label_2"));
-
-        gridLayout_2->addWidget(label_2, 0, 0, 1, 1);
-
-        gridLayoutWidget_3 = new QWidget(centralwidget);
-        gridLayoutWidget_3->setObjectName(QStringLiteral("gridLayoutWidget_3"));
-        gridLayoutWidget_3->setGeometry(QRect(380, 235, 401, 291));
-        gridLayout_3 = new QGridLayout(gridLayoutWidget_3);
-        gridLayout_3->setObjectName(QStringLiteral("gridLayout_3"));
-        gridLayout_3->setContentsMargins(0, 0, 0, 0);
-        requestImageButton = new QPushButton(gridLayoutWidget_3);
-        requestImageButton->setObjectName(QStringLiteral("requestImageButton"));
-
-        gridLayout_3->addWidget(requestImageButton, 2, 0, 1, 1);
-
-        viewImageButton = new QPushButton(gridLayoutWidget_3);
-        viewImageButton->setObjectName(QStringLiteral("viewImageButton"));
-
-        gridLayout_3->addWidget(viewImageButton, 3, 0, 1, 1);
-
-        imageStatusLabel = new QLabel(gridLayoutWidget_3);
-        imageStatusLabel->setObjectName(QStringLiteral("imageStatusLabel"));
-
-        gridLayout_3->addWidget(imageStatusLabel, 2, 1, 1, 1);
-
-        viewsNumLabel = new QLabel(gridLayoutWidget_3);
-        viewsNumLabel->setObjectName(QStringLiteral("viewsNumLabel"));
-
-        gridLayout_3->addWidget(viewsNumLabel, 3, 1, 1, 1);
-
-        imagesList = new QListView(gridLayoutWidget_3);
-        imagesList->setObjectName(QStringLiteral("imagesList"));
-
-        gridLayout_3->addWidget(imagesList, 1, 0, 1, 2);
-
-        getImagesButton = new QPushButton(gridLayoutWidget_3);
-        getImagesButton->setObjectName(QStringLiteral("getImagesButton"));
-
-        gridLayout_3->addWidget(getImagesButton, 0, 0, 1, 2);
-
-        getImagesButton->raise();
-        imagesList->raise();
-        requestImageButton->raise();
-        viewImageButton->raise();
-        viewsNumLabel->raise();
-        imageStatusLabel->raise();
-        gridLayoutWidget = new QWidget(centralwidget);
-        gridLayoutWidget->setObjectName(QStringLiteral("gridLayoutWidget"));
-        gridLayoutWidget->setGeometry(QRect(380, 10, 203, 218));
-        gridLayout = new QGridLayout(gridLayoutWidget);
-        gridLayout->setObjectName(QStringLiteral("gridLayout"));
-        gridLayout->setContentsMargins(0, 0, 0, 0);
-        my_images = new QListView(gridLayoutWidget);
-        my_images->setObjectName(QStringLiteral("my_images"));
-        QSizePolicy sizePolicy(QSizePolicy::Maximum, QSizePolicy::Expanding);
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName(QStringLiteral("verticalLayoutWidget"));
+        verticalLayoutWidget->setGeometry(QRect(10, 10, 781, 541));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        allImagesButton = new QPushButton(verticalLayoutWidget);
+        allImagesButton->setObjectName(QStringLiteral("allImagesButton"));
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(my_images->sizePolicy().hasHeightForWidth());
-        my_images->setSizePolicy(sizePolicy);
+        sizePolicy.setHeightForWidth(allImagesButton->sizePolicy().hasHeightForWidth());
+        allImagesButton->setSizePolicy(sizePolicy);
 
-        gridLayout->addWidget(my_images, 1, 0, 1, 1);
+        horizontalLayout->addWidget(allImagesButton);
 
-        label = new QLabel(gridLayoutWidget);
-        label->setObjectName(QStringLiteral("label"));
+        myImagesButton = new QPushButton(verticalLayoutWidget);
+        myImagesButton->setObjectName(QStringLiteral("myImagesButton"));
+        sizePolicy.setHeightForWidth(myImagesButton->sizePolicy().hasHeightForWidth());
+        myImagesButton->setSizePolicy(sizePolicy);
 
-        gridLayout->addWidget(label, 0, 0, 1, 1);
+        horizontalLayout->addWidget(myImagesButton);
 
-        Image_holder = new QLabel(centralwidget);
-        Image_holder->setObjectName(QStringLiteral("Image_holder"));
-        Image_holder->setGeometry(QRect(20, 10, 331, 481));
+        ediImageButton = new QPushButton(verticalLayoutWidget);
+        ediImageButton->setObjectName(QStringLiteral("ediImageButton"));
+        sizePolicy.setHeightForWidth(ediImageButton->sizePolicy().hasHeightForWidth());
+        ediImageButton->setSizePolicy(sizePolicy);
+
+        horizontalLayout->addWidget(ediImageButton);
+
+        requestImageButton = new QPushButton(verticalLayoutWidget);
+        requestImageButton->setObjectName(QStringLiteral("requestImageButton"));
+
+        horizontalLayout->addWidget(requestImageButton);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
+        imageView = new QLabel(verticalLayoutWidget);
+        imageView->setObjectName(QStringLiteral("imageView"));
+        QSizePolicy sizePolicy1(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(imageView->sizePolicy().hasHeightForWidth());
+        imageView->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_2->addWidget(imageView);
+
+        imagesList = new QListWidget(verticalLayoutWidget);
+        imagesList->setObjectName(QStringLiteral("imagesList"));
+        sizePolicy1.setHeightForWidth(imagesList->sizePolicy().hasHeightForWidth());
+        imagesList->setSizePolicy(sizePolicy1);
+
+        horizontalLayout_2->addWidget(imagesList);
+
+        editImageEntries = new QGridLayout();
+        editImageEntries->setObjectName(QStringLiteral("editImageEntries"));
+        usernameLabel = new QLineEdit(verticalLayoutWidget);
+        usernameLabel->setObjectName(QStringLiteral("usernameLabel"));
+        QSizePolicy sizePolicy2(QSizePolicy::Minimum, QSizePolicy::Fixed);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(usernameLabel->sizePolicy().hasHeightForWidth());
+        usernameLabel->setSizePolicy(sizePolicy2);
+
+        editImageEntries->addWidget(usernameLabel, 0, 1, 1, 1);
+
+        label_3 = new QLabel(verticalLayoutWidget);
+        label_3->setObjectName(QStringLiteral("label_3"));
+
+        editImageEntries->addWidget(label_3, 1, 0, 1, 1);
+
+        label_2 = new QLabel(verticalLayoutWidget);
+        label_2->setObjectName(QStringLiteral("label_2"));
+
+        editImageEntries->addWidget(label_2, 0, 0, 1, 1);
+
+        viewCountLabel = new QLineEdit(verticalLayoutWidget);
+        viewCountLabel->setObjectName(QStringLiteral("viewCountLabel"));
+        sizePolicy2.setHeightForWidth(viewCountLabel->sizePolicy().hasHeightForWidth());
+        viewCountLabel->setSizePolicy(sizePolicy2);
+
+        editImageEntries->addWidget(viewCountLabel, 1, 1, 1, 1);
+
+        updateEditsButton = new QPushButton(verticalLayoutWidget);
+        updateEditsButton->setObjectName(QStringLiteral("updateEditsButton"));
+
+        editImageEntries->addWidget(updateEditsButton, 2, 0, 1, 2);
+
+
+        horizontalLayout_2->addLayout(editImageEntries);
+
+
+        verticalLayout->addLayout(horizontalLayout_2);
+
         HomePage->setCentralWidget(centralwidget);
         menubar = new QMenuBar(HomePage);
         menubar->setObjectName(QStringLiteral("menubar"));
@@ -173,23 +173,21 @@ public:
 
     void retranslateUi(QMainWindow *HomePage)
     {
-        HomePage->setWindowTitle(QApplication::translate("HomePage", "MainWindow", Q_NULLPTR));
-        label_3->setText(QApplication::translate("HomePage", "Enter Views :", Q_NULLPTR));
-        update_views->setText(QApplication::translate("HomePage", "Update", Q_NULLPTR));
-        label_2->setText(QApplication::translate("HomePage", "Enter Username:", Q_NULLPTR));
+        HomePage->setWindowTitle(QApplication::translate("HomePage", "HomePage", Q_NULLPTR));
+        allImagesButton->setText(QApplication::translate("HomePage", "All Images", Q_NULLPTR));
+        myImagesButton->setText(QApplication::translate("HomePage", "My Images", Q_NULLPTR));
+        ediImageButton->setText(QApplication::translate("HomePage", "Edit Image", Q_NULLPTR));
         requestImageButton->setText(QApplication::translate("HomePage", "Request Image", Q_NULLPTR));
-        viewImageButton->setText(QApplication::translate("HomePage", "View Image", Q_NULLPTR));
-        imageStatusLabel->setText(QApplication::translate("HomePage", "No Image Requested", Q_NULLPTR));
-        viewsNumLabel->setText(QApplication::translate("HomePage", "No. of Views", Q_NULLPTR));
-        getImagesButton->setText(QApplication::translate("HomePage", "Get Images", Q_NULLPTR));
-        label->setText(QApplication::translate("HomePage", "Select One of your Images :", Q_NULLPTR));
-        Image_holder->setText(QApplication::translate("HomePage", "Image", Q_NULLPTR));
+        imageView->setText(QApplication::translate("HomePage", "TextLabel", Q_NULLPTR));
+        label_3->setText(QApplication::translate("HomePage", "Views Count", Q_NULLPTR));
+        label_2->setText(QApplication::translate("HomePage", "Add username", Q_NULLPTR));
+        updateEditsButton->setText(QApplication::translate("HomePage", "Update", Q_NULLPTR));
     } // retranslateUi
 
 };
 
 namespace Ui {
-    class HomePage: public Ui_HomePage {};
+    class HomePage: public Ui_MainWindow {};
 } // namespace Ui
 
 QT_END_NAMESPACE
