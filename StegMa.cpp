d34087e28de2504ab7e8d9e4af3644810447d566
faridaeid <faridaeid@aucegypt.edu>
2017-11-29 14:35:19 +0200

    Some changes in files

---
+++ StegMa.cpp
@@ -0,0 +1,4 @@
+//
+// Created by parallels on 11/29/17.
+//
+
---
+++ StegManager.h
@@ -0,0 +1,8 @@
+//
+// Created by parallels on 11/29/17.
+//
+
+#ifndef DISTRIBUTED_2_STEGMANAGER_H
+#define DISTRIBUTED_2_STEGMANAGER_H
+
+#endif //DISTRIBUTED_2_STEGMANAGER_H
