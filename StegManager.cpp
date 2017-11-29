fe7c314e80c18db6958f6bb4241cba60512c706e
karimhasebou <karimhasebou>
2017-11-29 20:00:49 +0200

    fixed unsigned & signed comparison warnings. exposed steg hide functions

--- StegManager.cpp
+++ StegManager.cpp
@@ -1,11 +1,13 @@
 #include "StegManager.h"
 using namespace std;
 
-mutex imgProtect;
-std::string DEFAULT = "DEFAULT.jpg";
+
 
 namespace stego_utils {
-    map<string, int> getAuthorizedUsersCount(std::string path);
+    std::mutex imgProtect;
+    std::string DEFAULT = "DEFAULT.jpg";
+
+
     string stegHideCmd(string cover, string hide);
     string stegUnhideCmd(string cover);
     int getUserCount(const string& user, const string& img);
@@ -13,10 +15,10 @@ namespace stego_utils {
     void updateCountInFile(map<string, int> list, std::string path);
 }
 
-map<string, int> stego_utils::getAuthorizedUsersCount(std::string path)
+std::map<std::string, int> stego::getAuthorizedUsersCount(std::string path)
 {
     map<string, int> list;
-    imgProtect.lock();
+    stego_utils::imgProtect.lock();
     ifstream imageListFile(path, ios::in);
 
     while(imageListFile.is_open()) {
@@ -36,13 +38,13 @@ map<string, int> stego_utils::getAuthorizedUsersCount(std::string path)
             imageListFile.close();
         }
     }
-    imgProtect.unlock();
+    stego_utils::imgProtect.unlock();
     return list;
 }
 
 string stego_utils::stegHideCmd(string cover, string hide)
 {
-    string cmd = "steghide embed -cf " + cover + " " +
+    string cmd = "steghide embed -cf " + cover + " -ef " +
         hide + " -p root";
     return cmd;
 }
@@ -55,30 +57,30 @@ string stego_utils::stegUnhideCmd(string cover)
 
 void stego::stegPicture(const string& imgCover, const string& imgHide)
 {
-    imgProtect.lock();
+    stego_utils::imgProtect.lock();
 
     std::string cmd = stego_utils::stegHideCmd(imgCover, imgHide);
     system(cmd.c_str());
 
-    imgProtect.unlock();
+    stego_utils::imgProtect.unlock();
 }
 
 void stego::unstegPicture(const string& img)
 {
-    imgProtect.lock();
+    stego_utils::imgProtect.lock();
 
-    std::string cmd = stego_utils::stegUnhideCmd(DEFAULT + img);
+    std::string cmd = stego_utils::stegUnhideCmd(stego_utils::DEFAULT + img);
     system(cmd.c_str());
     cmd = stego_utils::stegUnhideCmd(img);
     system(cmd.c_str());
 
-    imgProtect.unlock();
+    stego_utils::imgProtect.unlock();
 }
 
 int stego::getUserCount(const string& user, const string& img)
 {
     int count = -1;
-    std::map<std::string, int> allCount = stego_utils::getAuthorizedUsersCount(img);
+    std::map<std::string, int> allCount = stego::getAuthorizedUsersCount(img);
 
     if (allCount.count(user) > 0)
         count = allCount[user];
@@ -88,13 +90,13 @@ int stego::getUserCount(const string& user, const string& img)
 
 std::map<std::string, int> stego_utils::getAllCount(const std::string& img)
 {
-    std::map<std::string, int> usersCount = stego_utils::getAuthorizedUsersCount(img);
+    std::map<std::string, int> usersCount = stego::getAuthorizedUsersCount(img);
     return usersCount;
 }
 
 void stego_utils::updateCountInFile(map<string, int> list, std::string path)
 {
-    imgProtect.lock();
+    stego_utils::imgProtect.lock();
     ofstream imageListFile(path, ios::out);
     
     if(imageListFile.is_open()){
@@ -104,21 +106,43 @@ void stego_utils::updateCountInFile(map<string, int> list, std::string path)
             imageListFile<<entry.first<<" "<<entry.second<<endl;
         }
 
-        long pos = imageListFile.tellp();
         
         imageListFile.close();
     }
-    imgProtect.unlock();
+    stego_utils::imgProtect.unlock();
 }
 
 void stego::updateUserCount(const std::string& img, const std::string& user, const int& count)
 {
-    std::map<std::string, int> allCount = stego_utils::getAuthorizedUsersCount(img);
+    std::map<std::string, int> allCount = stego::getAuthorizedUsersCount(img);
 
     if (allCount.count(user) > 0) {
         allCount[user] = count;
         stego_utils::updateCountInFile(allCount, img + ".txt");
         stego::stegPicture(img, img + ".txt");
-        stego::stegPicture(DEFAULT + img, img);
+        stego::stegPicture(stego_utils::DEFAULT + img, img);
+    }
+}
+
+void stego::updateCountInMap(map<std::string, int> list, std::string path)
+{
+    ofstream imageListFile(path, ios::out);
+    
+    if(imageListFile.is_open()){
+        
+        for(auto& entry : list){
+            
+            imageListFile<<entry.first<<" "<<entry.second<<endl;
+        }
+
+        //long pos = imageListFile.tellp();
+        
+        // if(pos > 0){
+            
+        //     imageListFile.seekp(pos - 1);
+        //     imageListFile.put(EOF);
+        // }
+
+        imageListFile.close();
     }
 }
